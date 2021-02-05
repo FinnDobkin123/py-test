d4e93f61ba403da686e594ff18cd6eca1120c0ab
FinnDobkin123 <76793532+FinnDobkin123@users.noreply.github.com>
2021-02-04 22:20:15 -0500

    Update insurance.py

--- insurance.py
+++ insurance.py
@@ -10,3 +10,16 @@ with open('insurance.csv', 'r') as insurance_file:
                                fieldnames = fieldnames)
     #for row in csv_reader:
         #print(row)
+
+#Determine smoker ages
+smoker = []
+non_smoker = []
+with open('insurance.csv', 'r') as insurance_file:
+    fieldnames = ['age', 'sex', 'bmi', 'children', 'smoke', 'region', 'charges']
+    csv_reader = csv.DictReader(insurance_file, delimiter = ',', skipinitialspace=True, 
+                               fieldnames = fieldnames)
+    for status in insurance_file:
+        if row['smoke'] == 'yes':
+            smoker.append(float(row['age']))
+        elif row['smoke'] == 'no':
+            non_smoker.append(float(row['age']))
