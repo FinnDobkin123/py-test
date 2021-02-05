10bbb94c9e6ada4f5a6ab9d291669b6617fb3cd2
FinnDobkin123 <76793532+FinnDobkin123@users.noreply.github.com>
2021-02-04 22:11:44 -0500

    Update insurance.py
    This file for practicing exploratory analysis in Python. Here, I am trying to look into the characteristics of smokers versus non-smokers.

--- insurance.py
+++ insurance.py
@@ -8,5 +8,5 @@ with open('insurance.csv', 'r') as insurance_file:
     fieldnames = ['age', 'sex', 'bmi', 'children', 'smoke', 'region', 'charges']
     csv_reader = csv.DictReader(insurance_file, delimiter = ',', skipinitialspace=True, 
                                fieldnames = fieldnames)
-    for row in csv_reader:
-        print(row)
+    #for row in csv_reader:
+        #print(row)
