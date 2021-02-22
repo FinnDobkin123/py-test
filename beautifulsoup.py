d69e3a556a6a64154893a1e224d6f31e88db5878
FinnDobkin123 <76793532+FinnDobkin123@users.noreply.github.com>
2021-02-21 21:41:50 -0500

    Create beautifulsoup.py

---
+++ beautifulsoup.py
@@ -0,0 +1,22 @@
+#Scrape data
+import requests
+from bs4 import BeautifulSoup
+url = "https://www.healthaffairs.org/toc/hlthaff/current"
+page = requests.get(url)
+soup = BeautifulSoup(page.content, 'html.parser')
+results = soup.find(id = 'pb-page-content')
+
+#Iterate through html of articles
+for result in results:
+    print(results, end = '\n'*2)
+print(results.prettify())
+tests = results.find_all('div')
+for test in tests:
+    print(test.text)
+    temp = test.text
+    temp2 = temp.split("")
+
+#Put into dataframe
+import numpy as np
+import pandas as pd
+from urllib.request import urlopen
